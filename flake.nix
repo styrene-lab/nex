f8dbfdcbc3921f4186d92c0a88f3e18b3525638f
Chris <72043878+cwilson613@users.noreply.github.com>
2026-04-16 23:11:10 -0400

    feat: add flake.nix for nix profile install
    Enables `nix profile install github:styrene-lab/nex` which the install
    script at nex.styrene.io/install.sh uses as the primary nix install path.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Package manager UX for nix-darwin + homebrew";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "nex";
+            version = "0.3.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            meta = with pkgs.lib; {
+              description = "Package manager UX for nix-darwin + homebrew";
+              homepage = "https://nex.styrene.io";
+              license = with licenses; [ mit asl20 ];
+              mainProgram = "nex";
+            };
+          };
+        }
+      );
+    };
+}
