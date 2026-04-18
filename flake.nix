f925b5046545ef8bfdc164ac1a908afeeaa9f585
Chris <72043878+cwilson613@users.noreply.github.com>
2026-04-17 21:10:26 -0400

    chore: bump v0.8.2

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "nex";
-            version = "0.8.1";
+            version = "0.8.2";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             meta = with pkgs.lib; {
